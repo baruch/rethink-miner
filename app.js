a2ba607be669e0495d45ba58d00872b40831def8
Baruch Even <baruch@ev-en.org>
2013-12-12 22:07:43 +0200

    Initial revision

---
+++ app.js
@@ -0,0 +1,56 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+var rdb = require('rethinkdb');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/q/:name', routes.q);
+
+var dbConfig = {
+  host : process.env.RDB_HOST || 'localhost',
+  port : parseInt(process.env.RDB_PORT) || 28015,
+  db   : process.env.RDB_DB || 'rethink_miner',
+};  
+
+// Using a single db connection for the app
+rdb.connect({host: dbConfig.host, port: dbConfig.port}, function(err, connection) {
+  if(err) {
+    console.log("ERROR: %s:%s", err.name, err.msg);
+    process.exit(1);
+  }
+  else {
+    // set up the default database for the connection
+    connection.use(dbConfig['db']);
+    // set up the module global connection
+    routes.connection = connection;
+    // start serving requests
+    http.createServer(app).listen(app.get('port'), function(){
+      console.log('Express server listening on port ' + app.get('port'));
+    }); 
+  }
+});
---
+++ routes/index.js
@@ -0,0 +1,98 @@
+var r = require('rethinkdb'),
+    debug = require('debug')('rdb')
+    self = this;
+
+
+debug = console.log
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+    r.table('queries').run(self.connection, function(err, cursor) {
+        cursor.toArray(function(err, results) {
+            if(err) {
+                debug("[ERROR] %s:%s\n%s", err.name, err.msg, err.message);
+                res.render('index', {title: 'No results', res: []});
+            }
+            else{
+                res.render('index', {title: 'Known Queries', res: results});
+            }
+        });
+    });
+};
+
+function prepare_table(fields_list, results) {
+        headers = []
+        fields = []
+        if (fields_list == null) {
+                fields = Object.keys(results[0])
+                headers = Object.keys(results[0])
+                if (fields.indexOf('reduction') != -1) {
+                        fields.pop() // Assume it is last
+                        headers.pop()
+                        red = Object.keys(results[0].reduction)
+                        red.forEach(function(field) {
+                                fields.push(function(f) {
+                                        return f['reduction'][field]
+                                })
+                                headers.push(field)
+                        })
+                }
+        } else {
+                fields_list.forEach(function(field) {
+                        fields.push(field[0])
+                        headers.push(field[1])
+                })
+        }
+        return [headers, fields];
+}
+
+exports.q = function(req, res) {
+        r.table('queries').get(req.params.name).run(self.connection, function(err, result) {
+                if (result == null) {
+                        res.redirect('/')
+                        return
+                }
+                query = result.query;
+                fields_list = result.fields
+                if (1) {
+                        
+                        try {
+                                q = eval(query)
+                        
+                                q.run(self.connection, function(err, cursor) {
+                                        cursor.toArray(function(err, results) {
+                                                if (err) {
+                                                        debug("[ERROR] %s:%s\n%s", err.name, err.msg, err.message);
+                                                        res.render('query', {name: req.params.name, code: query, res: []})
+                                                } else {
+                                                        d = prepare_table(fields_list, results);
+                                                        headers = d[0]
+                                                        fields = d[1]
+                                                        entries = []
+                                                        results.forEach(function(res) {
+                                                                entry = []
+                                                                fields.forEach(function(field) {
+                                                                        if (typeof field == "string") {
+                                                                                entry.push(res[field])
+                                                                        } else {
+                                                                                entry.push(field(res))
+                                                                        }
+                                                                });
+                                                                entries.push(entry);
+                                                        });
+                                                        res.render('query', {name: req.params.name, code: query, headers:headers, res: entries});
+                                                }
+                                        });
+                                });
+                        }
+                        catch (e) {
+                                res.render('query', {name: req.params.name, code: query, headers:['Failed to run query'], res: [[e.toString()]]})
+                        }
+                } else {
+                        res.render('query', {name: req.params.name, code: query, res: []});
+                }
+        });
+};
