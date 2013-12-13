958596157938794601e57403573464cab630bc7e
Baruch Even <baruch@ev-en.org>
2013-12-13 09:36:44 +0200

    Add test data to enable playing with it and allow easy testing

--- routes/index.js
+++ routes/index.js
@@ -59,7 +59,7 @@ exports.q = function(req, res) {
                 if (err) {
                         return res.render('error', {title: 'Error querying database', description: err})
                 }
-                if (result == null) {
+                if (result === null) {
                         return res.render('error', {title: 'No results found for query "' + req.params.name + '"'})
                 }
                 query = result.query;
@@ -104,9 +104,84 @@ exports.q = function(req, res) {
         });
 };
 
+function test_data() {
+  return [
+  {
+    'serial': 'DISK1',
+    'temperature': 28,
+    'reallocations': 0
+  },
+  {
+    'serial': 'DISK2',
+    'temperature': 38,
+    'reallocations': 20
+  },
+  {
+    'serial': 'DISK3',
+    'temperature': 25,
+    'reallocations': 120
+  },
+  {
+    'serial': 'DISK4',
+    'temperature': 25,
+    'reallocations': 72
+  },
+  {
+    'serial': 'DISK5',
+    'temperature': 28,
+    'reallocations': 4096
+  },
+  {
+    'serial': 'DISK6',
+    'temperature': 42,
+    'reallocations': 1
+  },
+  {
+    'serial': 'DISK7',
+    'temperature': 27,
+    'reallocations': 1025
+  },
+  {
+    'serial': 'DISK8',
+    'temperature': 14,
+    'reallocations': 2
+  },
+  {
+    'serial': 'DISK9',
+    'temperature': 33,
+    'reallocations': 190
+  }
+  ];
+}
+
+function test_queries() {
+  return [
+  {
+    'name': 'Temperature Average',
+    'query': "r.db('rethink_miner').table('test').pluck('temperature').avg()"
+  },
+  ];
+}
+
 exports.setupDB = function (conn) {
-        r.dbCreate('rethink_miner').run(conn, function(err, result) {
-                r.db('rethink_miner').tableCreate('queries').run(conn, function(err, result) {
+        dbName = 'rethink_miner';
+        
+        r.dbCreate(dbName).run(conn, function(err, result) {
+                r.db(dbName).tableCreate('queries', {primaryKey: 'name'}).run(conn, function(err, result) {
+                  if (result && result.created === 1) {
+                    r.db(dbName).tableCreate('test').run(conn, function(err, result) {
+                      r.db(dbName).table('test').insert(test_data()).run(conn, function(err, result) {
+                        if (result) {
+                          debug("Inserted %s sample test entries into table 'test' in db '%s'", result.inserted, dbName)
+                        }
+                      })
+                      r.db(dbName).table('queries').insert(test_queries()).run(conn, function(err, result) {
+                        if (result) {
+                          debug("Inserted %s sample queries into table 'queries' in db '%s'", result.inserted, dbName)
+                        }
+                      })
+                    })
+                  }
                 })
         })
 }
