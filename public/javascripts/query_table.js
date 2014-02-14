f098b78c0a7b98ddba4877142d84f0b2683c2ee8
Baruch Even <baruch@ev-en.org>
2014-02-14 14:39:12 +0200

    Add validation for filter fields

--- public/javascripts/query_table.js
+++ public/javascripts/query_table.js
@@ -13,4 +13,39 @@ $(document).ready(function() {
   $('#order_select').multiselect({
     numberDisplayed: 6
   });
+
+  $('.table-filter').each(function (element) {
+    var field = $(this);
+    function check_filter(eventObject) {
+      var fparent = field.parent();
+      var val = field.val().replace(/^\s+|\s+$/g, '');
+      if (val == '') {
+        fparent.removeClass('has-success');
+        fparent.removeClass('has-error');
+        return;
+      }
+
+      function has_success() {
+        fparent.removeClass('has-error');
+        fparent.addClass('has-success');
+      }
+
+      m = val.match(/^(>|>=|<=|<)\s*[0-9]+$/);
+      if (m) {
+        has_success();
+        return;
+      }
+
+      m = val.match(/^(=|{|}|regex:)\s*\S.*$/)
+      if (m) {
+        has_success();
+        return;
+      }
+
+      fparent.removeClass('has-success');
+      fparent.addClass('has-error');
+    }
+    $(this).keyup(check_filter);
+    check_filter(null);
+  });
 });
